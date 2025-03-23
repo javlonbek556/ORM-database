6ea70ca7044aff1f7fa623936919e828559e2daa
javlonbek556 <javlonzdess@gmail.com>
2025-03-23 17:50:15 +0500

    request validation cdes there

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'price' => 'required|numeric',
+        ];
+        if ($this->nullable('price')){
+            return[
+                'price'=>'maydonchami toldiring'
+            ];
+        }
+        if ($this->nullable('title')){
+            return[
+                'price'=>'maydonchami toldiring'
+            ];
+        }
+        if ($this->nullable('author')){
+            return[
+                'price'=>'maydonchami toldiring'
+            ];
+        }
+    }
+}
